3653eaf202d9bf876b4808b92a8e1b49d9e4a4a3
michaelbroadbent <michael.m.broadbent@gmail.com>
2026-02-26 18:07:02 -0500

    fix vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coaching-hotboard/', // Change this to match your repo name
+  base: process.env.VERCEL ? '/' : '/coaching-hotboard/', // Change this to match your repo name
 })
